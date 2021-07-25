45a11a74a3467dd826f470df0c56c2fe51f1a049
Cowboyserg <skuzmenkov@list.ru>
2021-07-25 13:35:23 +0300

    new find of windows2

--- try_window_locate.py
+++ try_window_locate.py
@@ -1,27 +1,39 @@
 import win32gui
 
 
-def enumHandler(hwnd, lParam):
-    if win32gui.IsWindowVisible(hwnd):
-        if 'Total Commander' in win32gui.GetWindowText(hwnd):
-            return hwnd
-def get_total_com_box():
+class Win:
+    def __init__(self):
+        self.hwnd = None
+        win32gui.EnumWindows(self.enumHandler, None)
 
+    def enumHandler(self, hwnd, lParam):
+        if win32gui.IsWindowVisible(hwnd):
+            if 'Total Commander' in win32gui.GetWindowText(hwnd):
+                # print(123)
+                self.hwnd = hwnd
 
-def callback(hwnd, extra):
-    rect = win32gui.GetWindowRect(hwnd)
-    x = rect[0]
-    y = rect[1]
-    w = rect[2] - x
-    h = rect[3] - y
-    if "Total Commander" in win32gui.GetWindowText(hwnd):
-        print(f"Window {win32gui.GetWindowText(hwnd)}:")
-        print(f"\tLocation: ({x}, {y})")
-        print(f"\t    Size: ({w}, {h})")
-        return (x,y),(w,h)
+    def callback(self, hwnd, extra):
+        rect = win32gui.GetWindowRect(hwnd)
+        x = rect[0]
+        y = rect[1]
+        w = rect[2] - x
+        h = rect[3] - y
+        if "Total Commander" in win32gui.GetWindowText(hwnd):
+            print(f"Window {win32gui.GetWindowText(hwnd)}:")
+            print(f"\tLocation: ({x}, {y})")
+            print(f"\t    Size: ({w}, {h})")
+            return (x,y),(w,h)
 
+    def get_window_box(self):
+        rect = win32gui.GetWindowRect(hwnd)
+        x = rect[0]
+        y = rect[1]
+        w = rect[2] - x
+        h = rect[3] - y
+        return x, y, w, h
 
-hwnd = win32gui.EnumWindows(enumHandler, None)
-
-print(win32gui.EnumWindows(callback, None))
-# print(callback(hwnd, None))
+if __name__ == "__main__":
+    print(11111111111)
+    w = Win()
+    hwnd = w.hwnd
+    print(w.get_window_box())
